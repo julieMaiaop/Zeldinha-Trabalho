aa261b098189fc0f115e494e1a156c93af981b2f
elp28 <elpbr2807@gmail.com>
2025-03-25 16:09:24 -0300

    player fase 1

---
+++ zelda/Assets/Scripts/Playerfase1.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Playerfase1 : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+        moveInput.Normalize();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = moveInput * moveSpeed;
+    }
+}
+

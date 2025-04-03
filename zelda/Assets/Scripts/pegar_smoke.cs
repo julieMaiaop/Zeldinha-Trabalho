fdf0fc27bad67f66b27337e547b35fbec126d4e8
elp28 <elpbr2807@gmail.com>
2025-04-02 22:18:13 -0300

    mascara de gás

--- zelda/Assets/Scripts/pegar_smoke.cs
+++ zelda/Assets/Scripts/pegar_smoke.cs
@@ -1,32 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class pegar_smoke : MonoBehaviour
 {
+    public GameObject mascara;
 
-    public TMP_Text smokeText;
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Destroy(gameObject);
-            smokeText.text = "Utilize Q para usar a granada de gás";
+            mascara.SetActive(true); 
         }
     }
-   
 }
